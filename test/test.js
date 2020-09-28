7f1d8c5355132f8e0cb37fa90bd1ab0e520a4616
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-09-28 09:28:37 +0900

    単純にreadが許可されているだけのテストはDBに何も入ってなく(一番上のコレクションすらない)ても成功する

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 const assert = require('assert');
+const firebase = require('@firebase/rules-unit-testing');
+
+const MY_PROJECT_ID = 'practice-da34f';
 
 describe("Our social app", () => {
   it("Understands basic addition", () => {
     assert.strictEqual(2 + 2, 4);
   });
+
+  it("Can read items in the read-only collection", async () => {
+    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
+    const testDoc = db.collection("examanations").doc('docA');
+    await firebase.assertSucceeds(testDoc.get());
+  })
 });
