46fbe3aa2532b1f1034d96b9675697bd583827f5
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-09-28 10:10:00 +0900

    自分のuserIdのusersは更新できるが、他人のは無理

--- test/test.js
+++ test/test.js
@@ -12,5 +12,25 @@ describe("Our social app", () => {
     const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
     const testDoc = db.collection("examanations").doc('docA');
     await firebase.assertSucceeds(testDoc.get());
-  })
+  });
+
+  it("Can't write items in the read-only collection", async () => {
+    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
+    const testDoc = db.collection("examanations").doc('docB');
+    await firebase.assertFails(testDoc.set({ foo: "bar" }));
+  });
+
+  it("Can write to a user document with the same ID as our user", async () => {
+    const myAuth = { uid: "user_abc", email: "abc@gmail.com" };
+    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID, auth: myAuth }).firestore();
+    const testDoc = db.collection("users").doc('user_abc');
+    await firebase.assertSucceeds(testDoc.set({ foo: "bar" }));
+  });
+
+  it("Can't write to a user document with a differnt ID as our user", async () => {
+    const myAuth = { uid: "user_abc", email: "abc@gmail.com" };
+    const db = firebase.initializeTestApp({ projectId: MY_PROJECT_ID, auth: myAuth }).firestore();
+    const testDoc = db.collection("users").doc('user_xyz');
+    await firebase.assertFails(testDoc.set({ foo: "bar" }));
+  });
 });
