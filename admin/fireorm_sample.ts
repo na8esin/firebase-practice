169996c5300c45078e14732975c8cd159f6b21db
na8esin <na8esin.mpst@gmail.com>
2021-02-05 13:48:27 +0900

    fireormの導入検討: サブコレクションをidで問い合わせたいとき親コレクションへの問い合わせが余分になる

---
+++ admin/fireorm_sample.ts
@@ -0,0 +1,49 @@
+import * as admin from 'firebase-admin';
+import * as fireorm from 'fireorm';
+import { Collection, getRepository, SubCollection, ISubCollection } from 'fireorm';
+
+const home = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+
+
+var serviceAccount = require(`${home}/Downloads/practice-da34f-firebase-adminsdk-ueykj-053ff9bed4.json`);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://practice-da34f.firebaseio.com"
+});
+
+const firestore = admin.firestore();
+fireorm.initialize(firestore);
+
+class Detail {
+  id!: string;
+  title?: string;
+}
+
+@Collection('publics')
+class Public {
+  id!: string;
+  name?: string;
+
+  @SubCollection(Detail, 'details')
+  details?: ISubCollection<Detail>
+}
+
+
+async function main() {
+  const publicRepository = getRepository(Public);
+
+  // ここで一回リクエスト
+  // ただサブコレクションはまだ問い合わせしてない
+  const publics = await publicRepository.find();
+
+  const detailsPending = publics.map((e) => {
+    return e.details?.find();
+  });
+
+  // これで問い合わせされるが、public分が一回だけ余分
+  const details = await Promise.all(detailsPending);
+
+}
+
+main();
