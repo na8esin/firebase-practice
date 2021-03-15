a31f8e7e7d804a8067267594eab1befadd3398fc
na8esin <na8esin.mpst@gmail.com>
2021-03-15 13:40:06 +0900

    tokensに型を付ける。arrayRemove()の注意事項を追加

--- admin/firestore/array_map_delete.ts
+++ admin/firestore/array_map_delete.ts
@@ -2,6 +2,9 @@ import * as admin from 'firebase-admin';
 // serviceAccountを取得
 import { getInitializeAppOptions } from '../getInitializeAppOptions';
 
+/** とりあえずの型 */
+type TokenDevice = { token: String, device: 'android' | 'apns' | 'webpush' | 'unknown' };
+
 admin.initializeApp(getInitializeAppOptions());
 const firestore = admin.firestore();
 const docRef = firestore
@@ -10,20 +13,22 @@ const docRef = firestore
 main();
 
 async function setup() {
-  await
-    docRef.set({
-      tokens: [
-        { token: 'aaa', device: 'android' },
-        { token: 'bbb', device: 'apns' }
-      ]
-    });
+  const tokens: TokenDevice[] =
+    [
+      { token: 'aaa', device: 'android' },
+      { token: 'bbb', device: 'apns' }
+    ]
+
+  await docRef.set({ tokens });
 }
 
 async function main() {
   await setup();
   await docRef
     .update({
-      // { token: 'bbb' }だけだと消えない
+      // { token: 'bbb' } だけだと削除できない
+      // 引数にundefinedを入れると例外が発生する
+      // Error: Element at index 0 is not a valid array element. Cannot use "undefined" as a Firestore value
       "tokens": admin.firestore.FieldValue.arrayRemove({ token: 'bbb', device: 'apns' })
     });
   console.log((await docRef.get()).data());
