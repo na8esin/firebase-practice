50db6f071295ec039cbbc0a276ffcb8970c26805
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-12 12:02:15 +0900

    ベタがきした文字列をcsvにパースするサンプル

---
+++ admin/csv/csv-sample.ts
@@ -0,0 +1,13 @@
+ import * as parse from 'csv-parse/lib/sync';
+import * as  assert from 'assert';
+
+const input = `
+"key_1","key_2"
+"value 1","value 2"
+`
+const records = parse(input, {
+  columns: true,
+  skip_empty_lines: true
+})
+console.log(records);
+assert.deepStrictEqual(records, [{ key_1: 'value 1', key_2: 'value 2' }])
