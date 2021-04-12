6a2f8c22c453b9f9c2c0e98471ed16b5bf25c39b
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-12 17:56:31 +0900

    実ファイルを読み込んで出力。さらにcsvをparse

--- admin/csv/read-file-csv.ts
+++ admin/csv/read-file-csv.ts
@@ -1,8 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as parse from 'csv-parse/lib/sync';
 
-(async function(){
-  const content = await fs.promises.readFile(path.join(__dirname,'./sample.csv'));
-  console.log(content.toString());
+(async function () {
+  const content =
+    await fs.promises.readFile(path.join(__dirname, './sample.csv'));
+  const input = content.toString();
+
+  const records = parse(input, {
+    columns: true,
+    skip_empty_lines: true
+  })
+  console.log(records);
 })();
-
