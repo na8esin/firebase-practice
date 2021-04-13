d8375efc89980f3ad818820e7d3c39805a16daa2
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-13 09:50:59 +0900

    サンプルコードのURLを追記

--- admin/csv/read-file-csv.ts
+++ admin/csv/read-file-csv.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as parse from 'csv-parse/lib/sync';
 
+// https://github.com/adaltas/node-csv-parse/blob/master/samples/recipe.file.js
+
 (async function () {
   const content =
     await fs.promises.readFile(path.join(__dirname, './sample.csv'));
