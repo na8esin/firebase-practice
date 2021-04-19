a79ba16062924c71b9bea4cdb01ecd401230626b
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-19 17:20:19 +0900

    batchWriteした時のテストコードをしっかり書いてみる

--- functions/README.md
+++ functions/README.md
@@ -1,2 +1,5 @@
 ## powershellで npm testに引数を渡す(windows)
 `npm test .\src\index.spec.ts  "--" "-g" "pattern"`
+
+## サンプルコードは chai assert
+https://github.com/firebase/functions-samples/blob/d3cbed16f2bcdf087fc7109e037c48459d30b425/quickstarts/uppercase/functions/test/test.online.js#L23
--- functions/src/book/book.spec.ts
+++ functions/src/book/book.spec.ts
@@ -1,7 +1,12 @@
-import { fireormInitialize } from '../fireorm-initialize';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 import { getRepository, getCustomRepository } from 'fireorm';
+import { fireormInitialize } from '../fireorm-initialize';
 import { BooksRepository } from './books-repository';
 import { Book } from './book';
+
+chai.use(chaiAsPromised);
+const assert = chai.assert;
 
 // https://github.com/wovalle/fireorm/issues/232
 BooksRepository.name;
@@ -9,9 +14,10 @@ BooksRepository.name;
 fireormInitialize();
 
 describe("book", () => {
+  const bookRepository = getRepository(Book);
   it("Get books", async () => {
-    const repository = getRepository(Book);
-    const books = await repository.find();
+
+    const books = await bookRepository.find();
     console.log(books);
   });
 
@@ -21,10 +27,28 @@ describe("book", () => {
     },
     {
       title: "Readable Code"
-    },
-    ];
+    },];
+
+    // batchWriteの内部でtestdataが変更されるので
+    // ディープコピー
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#deep_clone
+    const copyTestdata = JSON.parse(JSON.stringify(testdata));
 
     const repository = getCustomRepository(Book) as BooksRepository;
     await repository.batchWrite(testdata);
+    const books = await repository.find();
+    const bookTitles = books.map((e) => { return { "title": e.title ?? "" } })
+      .sort((a, b) => {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+      });
+
+    assert.deepEqual(copyTestdata, bookTitles);
+  });
+
+  after(async () => {
+    const books = await bookRepository.find();
+    books.forEach(async (e) => await bookRepository.delete(e.id));
   });
 });
