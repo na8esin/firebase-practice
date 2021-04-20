28f61a99c27902c6d65acd6861c6950d64c04760
渡辺孝之 <na8esin.mpst@gmail.com>
2021-04-20 11:20:16 +0900

    エンドポイント storage.object().onFinalizeを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,10 @@
 import * as functions from "firebase-functions";
+import { ImporterService } from './importer/importer.service';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// 個人的なプロジェクトではBlazeじゃないのでデプロイはしたことがないが、
+// ほぼ同等のコードを業務でデプロイしたことがある
+
+export const csvImport
+  = functions.storage.object().onFinalize(async (metadata) => {
+    ImporterService.import(metadata);
+  });
