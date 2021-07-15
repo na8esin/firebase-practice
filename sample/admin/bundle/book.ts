8d30e6bebf38d18d5703dda6ff42b0cc40c0a11a
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-15 14:19:37 +0900

    stories/storiesに似た書き方で作ったbundleみてみたけどやっぱり意味なし

--- sample/admin/bundle/book.ts
+++ sample/admin/bundle/book.ts
@@ -6,23 +6,26 @@ import { open } from 'fs/promises';
   const bundleId = "latest-books";
   const bundle = firestore.bundle(bundleId);
 
+  // リファレンスで↓みたいな書き方があったけど、bundleファイルをみた限りでは間違った書き方
+  // 本当に、booksの下にbooksっていうidのdocがあればいいけど。
   var docSnapshot = await firestore.doc('books/books').get();
   var querySnapshot = await firestore.collection('books').get();
 
   if (!docSnapshot.exists) {
     console.log('docSnapshot not exists');
-    return;
+    //return;
   }
 
   // Build the bundle
   // Note how querySnapshot is named "latest-books-query"
-  var bundleBuffer = bundle.add(docSnapshot) // Add a document
-    .add('latest-books-query', querySnapshot) // Add a named query.
+  var bundleBuffer = bundle
+    .add(docSnapshot) // Add a document
+    //.add('latest-books-query', querySnapshot) // Add a named query.
     .build();
 
   let filehandle;
   try {
-    filehandle = await open('./public/latest-books-query.txt', 'a+');
+    filehandle = await open('./public/latest-books-query_.txt', 'a+');
     filehandle.write(bundleBuffer);
   } finally {
     await filehandle?.close();
