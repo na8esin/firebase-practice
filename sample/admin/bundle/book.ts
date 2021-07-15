3a6bc05f783587748bb99ad7a83eb0b06d623fcb
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-15 11:16:36 +0900

    firestore バンドルを作ってファイルに書き出す

---
+++ sample/admin/bundle/book.ts
@@ -0,0 +1,25 @@
+import { getDb } from '../getInitializeAppOptions';
+import { open } from 'fs/promises';
+
+(async function () {
+  const firestore = getDb();
+  const bundleId = "latest-books";
+  const bundle = firestore.bundle(bundleId);
+
+  var docSnapshot = await firestore.doc('books/books').get();
+  var querySnapshot = await firestore.collection('books').get();
+
+  // Build the bundle
+  // Note how querySnapshot is named "latest-books-query"
+  var bundleBuffer = bundle.add(docSnapshot) // Add a document
+    .add('latest-books-query', querySnapshot) // Add a named query.
+    .build();
+
+  let filehandle;
+  try {
+    filehandle = await open('./latest-books-query.txt', 'a+');
+    filehandle.write(bundleBuffer);
+  } finally {
+    await filehandle?.close();
+  }
+})();
