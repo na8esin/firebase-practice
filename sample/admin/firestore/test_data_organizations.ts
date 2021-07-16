7d12f3cfb94f93b0e6f7553581e82cf8555e0f9c
渡辺孝之 <na8esin.mpst@gmail.com>
2021-07-16 12:23:48 +0900

    新しくorganizationsスキーマを作成

---
+++ sample/admin/firestore/test_data_organizations.ts
@@ -0,0 +1,10 @@
+import { getDb } from '../getInitializeAppOptions';
+
+const db = getDb();
+
+const organizationsRef = db.collection('organizations');
+
+(async function () {
+  const ret = await organizationsRef.doc()
+    .set({ name: "org1" });
+})();
