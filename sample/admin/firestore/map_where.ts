3fef5022ee9a084991f7754b22ccec495789f500
渡辺孝之 <na8esin.mpst@gmail.com>
2021-09-01 11:48:18 +0900

    where map field

---
+++ sample/admin/firestore/map_where.ts
@@ -0,0 +1,19 @@
+import * as admin from 'firebase-admin';
+import { getDb } from '../getInitializeAppOptions';
+const db = getDb();
+
+/**
+ * mapの検索って普通にできる？
+ * https://firebase.google.com/docs/firestore/solutions/index-map-field#map_field_and_subfields
+ */
+
+const colRef = db.collection('publics');
+
+(async () => {
+  // データセット
+  await colRef.doc('map_where').set({ map_filed: { name: 'John' } });
+  await colRef.doc('map_where2').set({ map_filed: { name: 'Paul' } });
+
+  const snap = await colRef.where('map_filed.name', '==', 'Paul').get();
+  console.log(Array.from(snap.docs.map(e => e.data())));
+})();
