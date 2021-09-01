a6cc5038455ae0592915c1fca28a692855426826
渡辺孝之 <na8esin.mpst@gmail.com>
2021-09-01 14:13:30 +0900

    存在しないドキュメントをgetしても例外が発生したりはしない

---
+++ sample/admin/firestore/document-does-not-exist.ts
@@ -0,0 +1,10 @@
+import * as admin from 'firebase-admin';
+import { getDb } from '../getInitializeAppOptions';
+const db = getDb();
+
+const docRef = db.collection('publics').doc('document-does-not-exist');
+
+(async () => {
+  const snap = await docRef.get();
+  console.log(snap.data());
+})();
